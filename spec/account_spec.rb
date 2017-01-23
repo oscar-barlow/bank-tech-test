e0256f8a7f89d41726806ae29ae99d7cb7e9f2bf
Oscar Barlow <oscar.barlow@gmail.com>
2017-01-23 10:16:46 +0000

    test refactors

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,15 +2,15 @@ require 'account'
 
 describe Account do
 
-  let!(:account) { described_class.new({}) }
+  let!(:account) { described_class.new(balance: 1000) }
 
   it 'should initialize with a default balance of zero' do
-    expect(account.balance).to eq 0
+    empty_account = Account.new({})
+    expect(empty_account.balance).to eq 0
   end
 
   it 'should be possible to initialize an account with a balance' do
-    positive_account = Account.new(balance: 1000)
-    expect(positive_account.balance).to eq 1000
+    expect(account.balance).to eq 1000
   end
 
 end
