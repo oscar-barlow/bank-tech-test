680ed653e25bd372a446be72dc8b0d9fbf4ba010
Oscar Barlow <oscar.barlow@gmail.com>
2017-01-23 10:15:00 +0000

    account can initialize with a balance

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,4 +8,9 @@ describe Account do
     expect(account.balance).to eq 0
   end
 
+  it 'should be possible to initialize an account with a balance' do
+    positive_account = Account.new(balance: 1000)
+    expect(positive_account.balance).to eq 1000
+  end
+
 end
