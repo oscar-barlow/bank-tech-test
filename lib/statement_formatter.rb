8f6ecce549ff30bc386c55e6b8a17b97a27a114f
Oscar Barlow <oscar.barlow@gmail.com>
2017-01-23 14:22:18 +0000

    statement formatter initializes with an array of transactions

---
+++ lib/statement_formatter.rb
@@ -0,0 +1,8 @@
+class StatementFormatter
+  attr_reader :transactions
+
+  def initialize(transactions)
+    @transactions = transactions
+  end
+  
+end
---
+++ spec/statement_formatter_spec.rb
@@ -0,0 +1,17 @@
+require 'statement_formatter'
+
+describe StatementFormatter do
+
+  let!(:deposit) { double(credit: 1000.00, debit: nil, date: Date.new(2012,1,10), account_balance: 1000.00) }
+  let!(:big_deposit) { double(credit: 2000.00, debit: nil, date: Date.new(2012,1,13), account_balance: 3000.00) }
+  let!(:withdrawal) { double(credit: nil, debit: 500.00, date: Date.new(2012,1,14), account_balance: 2500.00) }
+
+  let!(:transactions) {[deposit, big_deposit, withdrawal]}
+
+  let!(:statement) {described_class.new(transactions)}
+
+  it 'should initialize with an array of transactions' do
+    expect(statement.transactions).to_not be_empty
+  end
+
+end
