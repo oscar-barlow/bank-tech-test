d111013c67ffcf5e9361f1b151e4c887be7212ac
Oscar Barlow <oscar.barlow@gmail.com>
2017-01-23 15:16:35 +0000

    extract formatting into separate methods

--- lib/statement_formatter.rb
+++ lib/statement_formatter.rb
@@ -10,37 +10,50 @@ class StatementFormatter
   end
 
   def print
-
     sorted_transactions = self.sort_transactions
 
     statement = "date       || credit || debit   || balance\n"
 
     sorted_transactions.each do |transaction|
-      row = []
-      date = "#{transaction.date.strftime("%d/%m/%Y")}".ljust(11) + "||"
-      row << date
-      if transaction.credit
-        amount = sprintf("%.2f", "#{transaction.credit}")
-        credit = "#{amount}".rjust(8) + "||"
-        row << credit
-      else
-        row << "        ||"
-      end
-      if transaction.debit
-        amount = sprintf("%.2f", "#{transaction.debit}")
-        credit = " #{amount}".ljust(9) + "||"
-        row << credit
-      else
-        row << "         ||"
-      end
-      amount = sprintf("%.2f", "#{transaction.account_balance}")
-      balance = " #{amount}".ljust(9)
-      row << balance
-      row << "\n"
+      row = [format_date(transaction),
+             credit_cell_creator(transaction),
+             debit_cell_creator(transaction),
+             format_balance(transaction),
+             "\n"
+            ]
       row = row.join
       statement += row
     end
     statement
+  end
+
+  private
+
+  def credit_cell_creator(transaction)
+    transaction.credit ? format_credit(transaction) : "        ||"
+  end
+
+  def debit_cell_creator(transaction)
+    transaction.debit ? format_debit(transaction) : "         ||"
+  end
+
+  def format_date(transaction)
+    "#{transaction.date.strftime("%d/%m/%Y")}".ljust(11) + "||"
+  end
+
+  def format_credit(transaction)
+    amount = sprintf("%.2f", "#{transaction.credit}")
+    "#{amount}".rjust(8) + "||"
+  end
+
+  def format_debit(transaction)
+    amount = sprintf("%.2f", "#{transaction.debit}")
+    " #{amount}".ljust(9) + "||"
+  end
+
+  def format_balance(transaction)
+    amount = sprintf("%.2f", "#{transaction.account_balance}")
+    " #{amount}".ljust(9)
   end
 
 end
